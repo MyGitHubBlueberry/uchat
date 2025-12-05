55eb9d577e742e489e83c5d6058fff6a0cffe748
Илья Свиридов <ilya.sviridov.05@gmail.com>
2025-12-05 21:17:21 +0100

    login

---
+++ uchat/uchat/Views/LoginWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace uchat.Views;
+
+public partial class LoginWindow : UserControl
+{
+    public LoginWindow()
+    {
+        InitializeComponent();
+    }
+}
