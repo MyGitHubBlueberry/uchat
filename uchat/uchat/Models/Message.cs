351c7154e421b914b34f841f0de70196d5bd7781
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2025-12-05 15:17:28 +0100

    feat: changes to models

--- uchat/uchat/Models/Message.cs
+++ uchat/uchat/Models/Message.cs
@@ -1,8 +1,11 @@
+using System;
+
 namespace uchat.Models;
 
-public record Message {
-    int id;
-    string text;
-    int timeSent;
-    int timeLastEdited;
+public record Message(string text) {
+    string text = text;
+    DateTime timeSent = DateTime.UtcNow; 
+    int id; // by db
+    int chatId; // also in db to tie messages to chats
+    DateTime timeLastEdited; // todo doesn't need to be in record. make it db column
 }
--- uchat/uchat_server/Models/NotificationModel.cs
+++ uchat/uchat_server/Models/NotificationModel.cs
@@ -1,3 +1,10 @@
+using uchat;
+
 namespace uchat_server.Models;
 
-public record NotificationModel();
+public record NotificationModel {
+    public string title { get; init; };
+    public string icon { get; init; } = "Uchat icon"; //todo change type
+    public string text { get; init; } = "text"; //todo change type
+    public DateTime time { get; init; }
+}
