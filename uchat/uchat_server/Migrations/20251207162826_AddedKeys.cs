c91f4fff2f4c02c0ce02d78434bb6871d0068111
WilfredoN <wilfredochannel@gmail.com>
2025-12-07 18:29:29 +0200

    feat: encryption keys migration

---
+++ uchat/uchat_server/Migrations/20251207162826_AddedKeys.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace uchat_server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "EncryptedKey",
+                table: "Chats",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "KeyIV",
+                table: "Chats",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EncryptedKey",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "KeyIV",
+                table: "Chats");
+        }
+    }
+}
