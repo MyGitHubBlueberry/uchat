903092d9bd34228949ae8ed5d76886052eacef4f
WilfredoN <wilfredochannel@gmail.com>
2025-12-07 20:07:48 +0200

    feat: message service

--- uchat/uchat_server/Services/IMessageService.cs
+++ uchat/uchat_server/Services/IMessageService.cs
@@ -4,6 +4,6 @@ namespace uchat_server.Services
 {
     public interface IMessageService
     {
-        Task<List<DbMessage>> GetChatMessagesAsync(int chatId, int skip = 0, int take = 50);
+        Task<List<DbMessage>> GetChatMessagesAsync(int chatId, int pageNumber = 1, int pageSize = 50);
     }
 }
