4d6b0851ebc9f4b77a64d9a73ecc0373799a0e75
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2025-12-07 15:25:52 +0100

    feat: added protection against occupied/reserved ports

--- uchat/uchat_server/Args/parser.cs
+++ uchat/uchat_server/Args/parser.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Net.Sockets;
+
 namespace uchat_server.Args;
 
 public static class Parser {
@@ -15,7 +18,12 @@ public static class Parser {
                 Console.WriteLine($"{program}: option '{args[0]}' has too few arguments");
                 Console.WriteLine($"Try '{program} --help' for more information.");
             } else if (int.TryParse(args[1], out port)) {
-                return true;
+                if (!IsPortAvailible(port)) {
+                    Console.WriteLine($"{program}: port '{args[1]}' is occupied or reserved");
+                    Console.WriteLine($"Try using another one.");
+                } else {
+                    return true;
+                }
             } else {
                 Console.WriteLine($"{program}: argument '{args[1]}' for option '{args[0]}' is incorrect");
                 Console.WriteLine($"Try '{program} --help' for more information.");
@@ -39,5 +47,16 @@ Options:
     --help    -h    Prints this message
 Example:
     uchat_server -p 8100");
+    }
+
+    static bool IsPortAvailible(int port) {
+        TcpListener l = new TcpListener(IPAddress.Loopback, port);
+        try {
+            l.Start();
+        } catch {
+            return false;
+        }
+        l.Stop();
+        return true;
     }
 }
