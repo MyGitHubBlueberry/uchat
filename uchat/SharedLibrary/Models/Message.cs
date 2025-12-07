dcb307a29770824246f0e19297242720d176cadd
WilfredoN <wilfredochannel@gmail.com>
2025-12-07 18:14:12 +0200

    temp: byte[] to string

--- uchat/SharedLibrary/Models/Message.cs
+++ uchat/SharedLibrary/Models/Message.cs
@@ -3,7 +3,7 @@ namespace SharedLibrary.Models;
 public class Message
 {
     public int Id { get; set; }
-    public byte[] Content { get; set; } = null!;
+    public string Content { get; set; } = null!;
     public string SenderName { get; set; } = null!;
     public int ChatId { get; set; }
     public DateTime Timestamp { get; set; } = DateTime.UtcNow;
