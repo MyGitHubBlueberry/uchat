5fdd82d2af5b1fb1d26ee672a1b9f323de3dd557
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2025-12-08 11:51:24 +0100

    feat: updated db with ecrypted message changes

---
+++ uchat/uchat_server/Migrations/20251208104348_EcriptedMessages.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace uchat_server.Migrations
+{
+    /// <inheritdoc />
+    public partial class EcriptedMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "CipheredText",
+                table: "Messages",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Iv",
+                table: "Messages",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CipheredText",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Iv",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "Messages",
+                type: "TEXT",
+                maxLength: 4000,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
